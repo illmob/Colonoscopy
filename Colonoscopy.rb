7e3fcc86678f835bd622d03389a56a332046db4d
illmob <42990096+illmob@users.noreply.github.com>
2019-02-28 22:45:05 -0700

    Update Colonoscopy.rb

--- Colonoscopy.rb
+++ Colonoscopy.rb
@@ -1,56 +1,76 @@
-ct  = "\x09\x09\x5b\x43\x6f\x64\x65\x4e\x61\x6d\x65\x5d\x3a\x09\x43\x6f"
+ct  = "\x09\x09\x5b\x43\x6f\x64\x65\x4e"
+ct += "\x61\x6d\x65\x5d\x3a\x09\x43\x6f"
 ct += "\x6c\x6f\x6e\x6f\x73\x63\x6f\x70\x79"
-pt  = "\x09\x09\x5b\x50\x72\x6f\x64\x75\x63\x65\x64\x5d\x3a\x09\x69\x6c"
-pt += "\x6c\x4d\x6f\x62\x20\x2d\x20\x51\x31\x2f\x32\x30\x31\x39"
-gt  = "\x09\x09\x5b\x4d\x6f\x74\x6f\x2f\x50\x53\x41\x5d\x3a\x09\x47\x65"
-gt += "\x74\x20\x79\x6f\x20\x73\x68\x69\x74\x20\x63\x68\x65\x63\x6b\x65\x64\x21"
+pt  = "\x09\x09\x5b\x50\x72\x6f\x64\x75"
+pt += "\x63\x65\x64\x5d\x3a\x09\x69\x6c"
+pt += "\x6c\x4d\x6f\x62\x20\x2d\x20\x51"
+pt += "\x31\x2f\x32\x30\x31\x39"
+gt  = "\x09\x09\x5b\x4d\x6f\x74\x6f\x2f"
+gt += "\x50\x53\x41\x5d\x3a\x09\x47\x65"
+gt += "\x74\x20\x79\x6f\x20\x73\x68\x69"
+gt += "\x74\x20\x63\x68\x65\x63\x6b\x65\x64\x21"
+aa  = "\x3a\x3b\x3a\x6f\x2c\x3b\x63\x3b"
+aa += "\x3a\x3b\x6c\x3a\x27\x78\x4d\x4d"
+aa += "\x3a\x64\x6f\x6c\x63\x63\x63\x6c\x78\x6b\x3a"
+ab  = "\x2e\x6c\x4b\x4e\x4b\x30\x30\x58\x57\x4d\x4d\x4d\x30\x6c\x2e"
+ac  = "\x63\x58\x4d\x4d\x57\x4b\x4f\x4f\x30\x4e\x4d\x4d\x4d\x6b\x2e"
+ad  = "\x2e\x3b\x6c\x64\x78\x78\x78\x6f"
+ab += "\x6b\x57\x4d\x57\x4f\x6f\x3a\x3b\x3a\x6c\x6b\x4e\x4d\x57\x64\x2e"
+ae  = "\x3b\x64\x30\x4e\x4d\x4d\x4d\x57\x58\x6b\x3a"
+af  = "\x2e\x2e\x2c\x2e\x2e\x2e\x2e\x2e\x2e\x2e\x3a\x4d\x4d\x6f"
+ag  = "\x3a\x4d\x4d\x30\x2e\x2c\x3a\x63\x63\x63\x6f\x78\x78\x6f\x6f\x3a\x2e"
+ah  = "\x2e\x63\x30\x4d\x4d\x4d\x4d\x4d\x4d\x4d\x57\x4b\x6b\x6c\x27"
+ai  = "\x2e\x63\x30\x4d\x4d\x4d\x4d\x4d\x4d\x4d\x57\x4b\x6b\x6c\x27"
+ai  = "\x6f\x57\x4d\x57\x6f"
+aj  = "\x2e\x6b\x4d\x4d\x4b\x3b"
+ak = "\x2e\x6c\x4b\x4d\x4d\x4e\x78\x78\x78\x78\x64\x6f\x3a\x27"
+
+
 banner = []
 banner.append('-'*80)
 banner.append('\x20'*5+"lXMMXk:."+"\x20"*51+",Kx,")
 banner.append('\x20'*7+".ckNMMNx:."+"\x20"*47+":0MMK;")
 banner.append('\x20'*11+"'lOWMMKd,"+"\x20"*46+"'0MMx")
 banner.append('\x20'*15+",oKMMM0l'"+"\x20"*36+".'."+"\x20"*5+"xMMd")
-banner.append('\x20'*18+".;dKMMWOl'"+"\x20"*21+"..,.......:MMo"+"\x20"*6+"0MM'")
-banner.append('\x20'*23+".;xXMMWOc."+"\x20"*14+":;:o,;c;:;l:'xMM:dolccclxk:")
-banner.append('\x20'*25+",xWMM0c."+"\x20"*14+",:'.'.'.."+"\x20"*3+":MM0.,:cccoxxoo:.")
+banner.append('\x20'*18+".;dKMMWOl'"+"\x20"*21+af+"\x20"*6+"0MM'")
+banner.append('\x20'*23+".;xXMMWOc."+"\x20"*14+aa)
+banner.append('\x20'*25+",xWMM0c."+"\x20"*14+",:'.'.'.."+"\x20"*3+ag)
 banner.append('\x20'*22+"'dNMMKl."+"\x20"*28+"lWMX."+"\x20"*7+"odooddd:")
 banner.append('\x20'*18+"'dNMMKl."+"\x20"*28+".cKMMk"+"\x20"*8+"kMMc"+"\x20"*1+".odok:")
 banner.append('\x20'*15+",dXMM0l."+"\x20"*21+".:c;;;:lxKMMNd."+"\x20"*7+";XMWl"+"\x20"*5+"ldlk.")
-banner.append('\x20'*12+",xNMM0c."+"\x20"*21+".c0MMMMMMMWKkl'"+"\x20"*6+".;dNMMk."+"\x20"*7+".klk;")
-banner.append('\x20'*9+"'dNMMKl."+"\x20"*21+".c0MMWx,"+"\x20"*1+"..."+"\x20"*3+".lKNK00XWMMM0l."+"\x20"*11+"klO,")
-banner.append('\x20'*7+",KMMKl."+"\x20"*22+":OMMWk;"+"\x20"*7+".lKMMNxxxxdo:'"+"\x20"*16+"O:0.")
+banner.append('\x20'*12+",xNMM0c."+"\x20"*21+ah+"\x20"*6+".;dNMMk."+"\x20"*7+".klk;")
+banner.append('\x20'*9+"'dNMMKl."+"\x20"*21+".c0MMWx,"+"\x20"*1+"..."+"\x20"*3+ab+"\x20"*11+"klO,")
+banner.append('\x20'*7+",KMMKl."+"\x20"*22+":OMMWk;"+"\x20"*7+ak+"\x20"*16+"O:0.")
 banner.append('\x20'*7+"dMM0'"+"\x20"*21+".cOWMNx;"+"\x20"*6+".'dKMMXd'"+"\x20"*25+".0ck")
 banner.append('\x20'*5+";MMO"+"\x20"*20+".c0MMNx,"+"\x20"*7+"'dNMMXo."+"\x20"*30+"llO'")
 banner.append('\x20'*6+"dMM,"+"\x20"*17+".cOMMWk,"+"\x20"*7+"'oXMMXo."+"\x20"*32+".Kcd")
 banner.append('\x20'*6+"lMMl"+"\x20"*17+"dWMMK."+"\x20"*6+".oXMMXo."+"\x20"*36+"k:K")
 banner.append('\x20'*6+".XMW;"+"\x20"*17+".xWMWo"+"\x20"*3+"lXMMXo'"+"\x20"*39+":ok'")
 banner.append('\x20'*7+".kMM0;"+"\x20"*17+".dWMWd"+"\x20"*2+"dWMNl"+"\x20"*40+".0cl")
-banner.append('\x20'*9+".kMMK;"+"\x20"*18+"oWMWx."+"\x20"*1+"oWMWo"+"\x20"*38+"0:0")
-banner.append('\x20'*10+".kMMK;"+"\x20"*18+"oWMWx."+"\x20"*1+"oWMWo"+"\x20"*37+"oc0.")
-banner.append('\x20'*12+".kMMK;"+"\x20"*18+"oWMWx."+"\x20"*1+"oWMWo"+"\x20"*35+".0cd")
+banner.append('\x20'*9+aj+"\x20"*18+"oWMWx."+"\x20"*1+ai+"\x20"*38+"0:0")
+banner.append('\x20'*10+".kMMK;"+"\x20"*18+"oWMWx."+"\x20"*1+ai+"\x20"*37+"oc0.")
+banner.append('\x20'*12+".kMMK;"+"\x20"*18+"oWMWx."+"\x20"*1+ai+"\x20"*35+".0cd")
 banner.append('\x20'*14+".xWMX:"+"\x20"*18+"lNMWx."+"\x20"*1+"lNMWd."+"\x20"*33+"dlO'")
 banner.append('\x20'*16+".xWMNc"+"\x20"*18+"cNMMk."+"\x20"*1+"cNMMk."+"\x20"*32+"Ock;")
 banner.append('\x20'*18+".xWMNc"+"\x20"*18+"cNMMk."+"\x20"*1+":KMMk."+"\x20"*31+"odddc,..")
 banner.append('\x20'*20+".xWMNc"+"\x20"*18+"cNMMk'"+"\x20"*1+";KMMk."+"\x20"*30+".lldxxx.")
 banner.append('\x20'*22+".xWMNc"+"\x20"*18+"cNMM0,"+"\x20"*1+";KMMk."+"\x20"*32+"..'")
 banner.append('\x20'*24+".dWMNl"+"\x20"*18+":KMM0;"+"\x20"*1+";0MMO'")
-banner.append('\x20'*27+"oWMWo"+"\x20"*18+",0MMK,"+"\x20"*1+",0MM0,")
-banner.append('\x20'*29+"oWMWo"+"\x20"*18+",0MMx"+"\x20"*1+",0MM0,")
-banner.append('\x20'*31+"oWMWo"+"\x20"*18+"xMMo"+"\x20"*3+",0MM0.")
+banner.append('\x20'*27+ai+"\x20"*18+",0MMK,"+"\x20"*1+",0MM0,")
+banner.append('\x20'*29+ai+"\x20"*18+",0MMx"+"\x20"*1+",0MM0,")
+banner.append('\x20'*31+ai+"\x20"*18+"xMMo"+"\x20"*3+",0MM0.")
 banner.append('\x20'*33+"lNMWd."+"\x20"*16+"WMK"+"\x20"*5+",XMW'")
 banner.append('\x20'*35+"cNMWx."+"\x20"*13+".WMK"+"\x20"*6+"'MM0"+ct)
 banner.append('\x20'*37+"cNMWx."+"\x20"*10+".0MM:"+"\x20"*7+"XMX"+pt)
 banner.append('\x20'*39+"cNMWx'"+"\x20"*6+".oNMW:"+"\x20"*7+";MMk"+gt)
-banner.append('\x20'*42+"cXMMWKOO0NMMMk."+"\x20"*7+"lWMX.")
-banner.append('\x20'*43+".;ldxxxokWMWOo:;:lkNMWd.")
-banner.append('\x20'*53+";d0NMMMWXk:")
+banner.append('\x20'*42+ac+"\x20"*7+"lWMX.")
+banner.append('\x20'*43+ad)
+banner.append('\x20'*53+ae)
 banner.append('-'*80)
 banner.append('[Initialize]')
 banner.append('')
 
-
 def initial
-
 	$counter = 0
 	$wsize = 500000
 	$infile = ARGV[0]
